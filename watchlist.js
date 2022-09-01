cb35202e5150c095e996998b09073da9b9e19bc5
Goran Stevanovic <stevanovicgoran@proton.me>
2022-09-01 20:48:07 +0200

    Remove movie from watchlist when Remove button is clicked
    See #18

--- watchlist.js
+++ watchlist.js
@@ -1,5 +1,23 @@
 const movieList = document.getElementById('movie-list');
 
-const movies = JSON.parse(localStorage.getItem('watchlist')) || [];
+let movies = JSON.parse(localStorage.getItem('watchlist')) || [];
 
 displayMovies(movies, 'Remove from Watchlist');
+
+function removeFromWatchlist(movieId) {
+  movies = movies.filter((movie) => movie.imdbID !== movieId);
+}
+
+function handleRemoveMovie(e) {
+  if (!e.target.classList.contains('movie__button')) {
+    return;
+  }
+
+  const movieId = e.target.dataset.id;
+
+  removeFromWatchlist(movieId);
+
+  displayMovies(movies, 'Remove from Watchlist');
+}
+
+movieList.addEventListener('click', handleRemoveMovie);
